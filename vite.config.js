f6c0473ba60d0c4b5490fa3a98c4d92dbdd77296
ZuevIevheniy <bigliver15@gmail.com>
2024-05-18 22:28:36 +0300

    Add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base:"/diploma_v3",
+  base:"/diploma_v3/",
   plugins: [react()],
   build: { sourcemap: true },
   resolve: {
